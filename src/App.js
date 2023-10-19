c3497f176e211224aaec2d37f27fa6828adb86cb
aartipp <121386624+aartipp@users.noreply.github.com>
2023-10-19 21:27:52 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Quizz from "./Components/Quizz";
+
+
+function App(){
+  return (
+   
+   <>
+   <Quizz/>
+   </>
+   
+    
+  );
+}
+
+export default App;
---
+++ src/Components/QuizzData.js
@@ -0,0 +1,43 @@
+const questions =[
+    {
+        questionText: "How many days are there in a week?",
+        answerOptions:[
+            {answerText:"2", isCorrect: false},
+            {answerText:"9", isCorrect: false},
+            {answerText:"7", isCorrect: true},
+            {answerText:"5", isCorrect: false},
+        ],
+    },
+
+    {
+        questionText: "Who is CEO of Tesla?",
+        answerOptions:[
+            {answerText:"Jeff Bezos", isCorrect: false},
+            {answerText:"Elon Musk", isCorrect: true},
+            {answerText:"Bill Gates", isCorrect: false},
+            {answerText:"Tony Stark", isCorrect: false},
+        ],
+    },
+
+    {
+        questionText: "Name te largest planet of our Solar system?",
+        answerOptions:[
+            {answerText:"Jupitar", isCorrect: true},
+            {answerText:"ploto", isCorrect: false},
+            {answerText:"Mercury", isCorrect: false},
+            {answerText:"Earth", isCorrect: false},
+        ],
+    },
+
+    {
+        questionText: "HTML Stands for?",
+        answerOptions:[
+            {answerText:"structuring Language", isCorrect: false},
+            {answerText:"Object Oriented Language", isCorrect: false},
+            {answerText:"Programming Language", isCorrect: false},
+            {answerText:"HyperText Markup Language", isCorrect: true},
+        ],
+    },
+]
+
+export default questions;
